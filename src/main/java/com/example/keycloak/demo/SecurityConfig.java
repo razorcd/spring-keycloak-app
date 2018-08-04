89ecfdefdc753e471c0592548062f0abdba5a614
Cristian Dugacicu <razorcd@yahoo.com>
2018-08-05 00:40:51 +0200

    cleanups

--- src/main/java/com/example/keycloak/demo/SecurityConfig.java
+++ src/main/java/com/example/keycloak/demo/SecurityConfig.java
@@ -6,21 +6,15 @@ import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticatio
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-//@EnableWebSecurity
 @KeycloakConfiguration
-//@Configuration
-//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
-//public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     /**
      * Registers the KeycloakAuthenticationProvider with the authentication manager.
@@ -55,9 +49,6 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/pets").permitAll()
                 .antMatchers( "/webjars/**").permitAll()
                 .anyRequest().authenticated()
-//            .and()
-//                .formLogin()
-//                .defaultSuccessUrl("/pets")
             .and()
                 .logout()
                 .logoutUrl("/logout")
