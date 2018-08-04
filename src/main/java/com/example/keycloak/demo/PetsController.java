9f9abe621a8e9d47a36f2a2d3d8c89804738187d
Cristian Dugacicu <razorcd@yahoo.com>
2018-08-04 23:28:47 +0200

    keycloak integration

--- src/main/java/com/example/keycloak/demo/PetsController.java
+++ src/main/java/com/example/keycloak/demo/PetsController.java
@@ -1,13 +1,16 @@
 package com.example.keycloak.demo;
 
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("pets")
--- src/main/java/com/example/keycloak/demo/SecurityConfig.java
+++ src/main/java/com/example/keycloak/demo/SecurityConfig.java
@@ -1,43 +1,66 @@
 package com.example.keycloak.demo;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@EnableWebSecurity
-//@KeycloakConfiguration
+//@EnableWebSecurity
+@KeycloakConfiguration
+//@Configuration
+//@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 //public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    /**
-//     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-//     */
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(keycloakAuthenticationProvider());
-//    }
-//
-//    /**
-//     * Defines the session authentication strategy.
-//     */
-//    @Bean
-//    @Override
-//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-//    }
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth){
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http
             .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/pets").permitAll()
                 .antMatchers( "/webjars/**").permitAll()
                 .anyRequest().authenticated()
-            .and()
-                .formLogin()
-                .defaultSuccessUrl("/pets")
+//            .and()
+//                .formLogin()
+//                .defaultSuccessUrl("/pets")
             .and()
                 .logout()
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/pets")
             .and()
                 .csrf().disable()
